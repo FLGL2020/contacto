8f94fc77a6458b314811087b18d3bbe964f71c5e
FLGL2020 <66809892+FLGL2020@users.noreply.github.com>
2020-06-18 22:25:58 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php 
+
+// llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+
+// Datos para el correo
+$destinatario = "flgl9504@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Telefono: $telefono \n";
+$carta .= "Mensaje: $mensaje";
+
+//Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('Location:mensaje.html');
+
+?>
